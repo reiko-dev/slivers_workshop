650875bc3dae317903e93efa6cdc7c7726fbb006
Vitor Lucas Pires Cordovil <lukassfifa@gmail.com>
2021-06-16 10:25:44 -0300

    Makes a initial formatting and changes frm ListTile to a Column-based style

--- lib/main.dart
+++ lib/main.dart
@@ -36,30 +36,73 @@ class WeeklyForecastList extends StatelessWidget {
   Widget build(BuildContext context) {
     final DateTime currentDate = DateTime.now();
     final TextTheme textTheme = Theme.of(context).textTheme;
-    final List<DailyForecast> forecasts = Server.getDailyForecastList();
 
-    return SingleChildScrollView(
-        child: Column(
-      children: forecasts.map((DailyForecast dailyForecast) {
+    //You can change SingleChildScrollView by a ListView.builder, in some cases this is more efficient.
+
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final dailyForecast = Server.getDailyForecastByID(index);
         return Card(
-          child: ListTile(
-            leading: Text(
-              dailyForecast.getDate(currentDate.day).toString(),
-              style: textTheme.headline4,
-            ),
-            title: Text(
-              dailyForecast.getWeekday(currentDate.weekday),
-              style: textTheme.headline5,
-            ),
-            subtitle: Text(dailyForecast.description),
-            trailing: Text(
-              '${dailyForecast.highTemp} | ${dailyForecast.lowTemp} F',
-              style: textTheme.subtitle2,
-            ),
+          child: Row(
+            children: [
+              SizedBox(
+                height: 200,
+                width: 200,
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    DecoratedBox(
+                      position: DecorationPosition.foreground,
+                      decoration: BoxDecoration(
+                        gradient: RadialGradient(
+                          colors: [
+                            Colors.grey[800]!,
+                            Colors.transparent,
+                          ],
+                        ),
+                      ),
+                      child: Image.network(
+                        dailyForecast.imageId,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        dailyForecast.getDate(currentDate.day).toString(),
+                        style: textTheme.headline2,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        dailyForecast.getWeekday(currentDate.weekday),
+                        style: textTheme.headline5,
+                      ),
+                      Text(dailyForecast.description),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '${dailyForecast.highTemp} | ${dailyForecast.lowTemp} F',
+                  style: textTheme.subtitle2,
+                ),
+              ),
+            ],
           ),
         );
-      }).toList(),
-    ));
+      },
+      itemCount: Server.getDailyForecastList().length,
+    );
   }
 }
 
